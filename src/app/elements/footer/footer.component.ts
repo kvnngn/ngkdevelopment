4ab8f5e94e8b590a9e00d6137afe1a2acaa3af93
knguyen <kevin3.nguyen@epitech.eu>
2020-04-02 05:50:01 +0200

    first commit

--- src/app/elements/footer/footer.component.ts
+++ src/app/elements/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { ThemeService } from '../../services/theme/theme.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
   currentYear : number;
